c7810d2a1e2d3aa60affb586441076c6f9c3d24b
qwekuspacely <achirigodfred@gmail.com>
2025-10-07 23:00:18 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Spacely Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Spacely Bot",
+  botOwner: 'Qweku Spacely', // Your name
+  ownerNumber: '+233534163467', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
